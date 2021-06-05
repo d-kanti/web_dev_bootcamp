1bd723172ef570bef7e208706fbff334c5a76699
d-kanti <dibyakanti22@gmail.com>
2021-06-05 21:07:10 +0530

    made a get req with axiom to get one joke and console-log it.

--- score_keeper/joke_fetcher/app.js
+++ score_keeper/joke_fetcher/app.js
@@ -1,1 +1,5 @@
-alert('hi there');
+const getJoke = async () => {
+    const header = {headers: { Accept : 'application/json'}};
+    const res = await axios.get('https://icanhazdadjoke.com',header);
+    console.log(res.data.joke);
+}
