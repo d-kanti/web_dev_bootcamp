28dcf0dbc4123eaf7e412e985f6a68291e727833
d-kanti <dibyakanti22@gmail.com>
2021-06-05 21:10:40 +0530

    button created and selectedd.

--- score_keeper/joke_fetcher/app.js
+++ score_keeper/joke_fetcher/app.js
@@ -1,5 +1,19 @@
+const but = document.getElementById('cbi');
+const liss = document.getElementById('jokes');
+
+
 const getJoke = async () => {
     const header = {headers: { Accept : 'application/json'}};
     const res = await axios.get('https://icanhazdadjoke.com',header);
     console.log(res.data.joke);
 }
+
+
+but.addEventListener('click',()=>{
+    alert('hi');
+})
+
+
+
+
+
