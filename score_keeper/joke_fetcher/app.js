e8e399db70f3b833174bb859de19811dd6cb07f9
d-kanti <dibyakanti22@gmail.com>
2021-06-05 21:18:34 +0530

    made ready, needed to use the await bcz a promise was returned.

--- score_keeper/joke_fetcher/app.js
+++ score_keeper/joke_fetcher/app.js
@@ -5,13 +5,18 @@ const liss = document.getElementById('jokes');
 const getJoke = async () => {
     const header = {headers: { Accept : 'application/json'}};
     const res = await axios.get('https://icanhazdadjoke.com',header);
-    console.log(res.data.joke);
+    return (res.data.joke);
+
+
 }
 
 
-but.addEventListener('click',()=>{
-    alert('hi');
-})
+but.addEventListener('click',async ()=>{
+    let joke = await getJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(joke);
+    jokes.append(newLI);
+}) 
 
 
 
