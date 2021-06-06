c9b892ac29343a72924d190a2bfa835606756f1e
d-kanti <dibyakanti22@gmail.com>
2021-06-06 13:13:16 +0530

    searching adds the image into body

--- tv_shows/app.js
+++ tv_shows/app.js
@@ -5,4 +5,7 @@ form.addEventListener('submit',async (e)=>{
     const Search_string = form.elements.q.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${Search_string}`)
     console.log(res.data[0].show.image.medium);
-});
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img)
+} );
