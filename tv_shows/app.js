f5097d5516368c00e5768042aa8df122e9f0afc8
d-kanti <dibyakanti22@gmail.com>
2021-06-06 13:05:01 +0530

    created and recovered the submitted value

--- tv_shows/app.js
+++ tv_shows/app.js
@@ -2,4 +2,5 @@ const form = document.querySelector("#form_s");
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     console.log("submitted");
-})
+    console.log("submitted value is:",form.elements.q.value);
+});
