1210be30c7bb30d51a8991529fd6675389a489bf
d-kanti <dibyakanti22@gmail.com>
2021-06-06 13:27:46 +0530

    able to add o9 images for search results

--- tv_shows/app.js
+++ tv_shows/app.js
@@ -1,11 +1,32 @@
 const form = document.querySelector("#form_s");
-form.addEventListener('submit',async (e)=>{
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
     console.log("submitted");
     const Search_string = form.elements.q.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${Search_string}`)
-    console.log(res.data[0].show.image.medium);
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img)
-} );
+    // console.log(res.data[0].show.image.medium);
+    await imageMaker(res.data);
+    // console.log(res.data);
+    // const img = document.createElement('IMG');
+    // img.src = res.data[0].show.image.medium;
+    // document.body.append(img)
+});
+
+let imageMaker = (arr) => {
+    count = 0
+    for (let i of arr) {
+
+        count += 1;
+        try {
+
+            const img = document.createElement('IMG');
+            img.src = i.show.image.medium;
+            document.body.append(img);
+        } catch { 
+            count -=1 ;
+            console.log('image not found for:', i.show.name) }
+        if (count === 9) {
+            break;
+        }
+    }
+}
