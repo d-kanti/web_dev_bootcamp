d93ac6624961e2c4187dc79755088345c943f8f6
d-kanti <dibyakanti22@gmail.com>
2021-06-06 13:37:22 +0530

    passing params in seperate object and all images are shown, not 9

--- tv_shows/app.js
+++ tv_shows/app.js
@@ -3,27 +3,21 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault();
     console.log("submitted");
     const Search_string = form.elements.q.value;
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${Search_string}`)
+    const config = { params: { q: Search_string }, headers: {} };
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
     imageMaker(res.data);
     form.elements.q.value = ""
 
 });
 
 let imageMaker = (arr) => {
-    count = 0
     for (let i of arr) {
-
-        count += 1;
         try {
-
             const img = document.createElement('IMG');
             img.src = i.show.image.medium;
             document.body.append(img);
-        } catch { 
-            count -=1 ;
-            console.log('image not found for:', i.show.name) }
-        if (count === 9) {
-            break;
+        } catch {
+            console.log('image not found for:', i.show.name)
         }
     }
 }
