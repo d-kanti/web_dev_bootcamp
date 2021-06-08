0da7e32febf41aa475da4448c253ff62d9e773d9
d-kanti <dibyakanti22@gmail.com>
2021-06-08 14:47:01 +0530

    added colored text

--- pack-npm/language_detector/index.js
+++ pack-npm/language_detector/index.js
@@ -1,5 +1,6 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 
 text = process.argv.slice(2).toString();
 
@@ -19,12 +20,13 @@ function tln(ar){
         try{
             if(ar.length>2){
 
-                return "The thing is: "+langs.where("3",ar[i][0]).name;
+                return "The thing is: ".green+" ".bgGreen+langs.where("3",ar[i][0]).name.black.bgGreen+" ".bgGreen;
             } else{
-                return "Sample too Small"
+
+                return "Sample too Small!!".bgRed.black
             }
         }catch{
-            console.log("trying");
+            console.log("trying....".yellow);
         }
     }
 }
