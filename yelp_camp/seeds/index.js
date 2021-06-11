3176424bbc901a57f68967d8f36079357e071c0a
d-kanti <dibyakanti22@gmail.com>
2021-06-11 21:52:25 +0530

    feeling confused

--- yelp_camp/seeds/index.js
+++ yelp_camp/seeds/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
-const Campground = require("./models/campground");
+const Campground = require("../models/campground");
 const { title } = require("process");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -17,3 +17,9 @@ db.on("error", console.error.bind(console, "connection error..."));
 db.once("open", () => {
   console.log("database connected....");
 });
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  const c = new Campground({ title: "purple field" });
+  await c.save();
+};
