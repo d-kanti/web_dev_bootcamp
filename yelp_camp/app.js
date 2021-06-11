abb89bfe36c5d6f0fc5341edbe78486fc874bbae
d-kanti <dibyakanti22@gmail.com>
2021-06-11 21:38:36 +0530

    database connected

--- yelp_camp/app.js
+++ yelp_camp/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error..."));
+db.once("open",() =>{
+    console.log("database connected....");
+})
 
 
 app.set('view engine','ejs');
@@ -19,5 +32,5 @@ app.get('/', (req,res)=>{
 
 
 app.listen(3000,()=>{
-    console.log("app listning on the port 3000....")
+    console.log("\n\n\napp listning on the port 3000....")
 })
