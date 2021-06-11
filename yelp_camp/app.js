4d027054129a3588fec98d127bd0b780d20acc5d
d-kanti <dibyakanti22@gmail.com>
2021-06-11 21:39:32 +0530

    campground model linked

--- yelp_camp/app.js
+++ yelp_camp/app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Campground =  require('./models/campground')
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
     useCreateIndex:true,
