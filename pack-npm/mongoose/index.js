05ee2f92c3a2d2a6e89598bb346ad7bd0fe8098e
d-kanti <dibyakanti22@gmail.com>
2021-06-12 16:49:46 +0530

    the new and edit page are now dinamic (the selection option) and are opetrated by one catagories variable passed as a variable.

--- pack-npm/mongoose/index.js
+++ pack-npm/mongoose/index.js
@@ -23,6 +23,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const catagories = ["fruit","vegetable", "dairy"];
+
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   console.log(products);
@@ -30,7 +32,7 @@ app.get("/products", async (req, res) => {
 });
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new",{catagories});
 });
 
 app.get("/products/:id", async (req, res) => {
@@ -52,14 +54,14 @@ app.get("/products/:id/edit", async (req, res) => {
   const product = await Product.findById(id);
   console.log(product);
 
-  res.render("products/edit", { product });
+  res.render("products/edit", { product,catagories });
 });
 
 app.put("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findByIdAndUpdate(id, req.body, {
     runValidators: true,
-    new: true
+    new: true,
   });
 
   res.redirect(`/products/${product._id}`);
