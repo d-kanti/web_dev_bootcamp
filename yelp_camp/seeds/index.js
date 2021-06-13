67ce3ae41c00c0ecaed9465833acb550a9263b64
d-kanti <dibyakanti22@gmail.com>
2021-06-13 15:25:56 +0530

    added title functionality for seeds

--- yelp_camp/seeds/index.js
+++ yelp_camp/seeds/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
+const { places, descriptors } = require("./seedHelper");
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -13,12 +15,15 @@ db.once("open", () => {
   console.log("database connected....");
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 9; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
     await camp.save();
   }
