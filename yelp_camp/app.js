43f4e2ba572e26645e6b50067e583ff0a6d2fe50
d-kanti <dibyakanti22@gmail.com>
2021-06-13 15:12:21 +0530

    created the app and linked to the database

--- yelp_camp/app.js
+++ yelp_camp/app.js
@@ -6,27 +6,35 @@ const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const { title } = require("process");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error..."));
 db.once("open", () => {
   console.log("database connected....");
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/camp", async (req, res) => {
-  const camp = new Campground({ title: "one camp" , description:"one or two"});
+  const camp = new Campground({ title: "two camp", description: "one or two" });
   await camp.save();
   res.send(camp);
 });
