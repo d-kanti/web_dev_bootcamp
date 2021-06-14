90dbbcfc2fb2d5dd025c96ca6386fc9879205be8
d-kanti <dibyakanti22@gmail.com>
2021-06-14 23:45:05 +0530

    updated the campground model

--- yelp_camp/models/campground.js
+++ yelp_camp/models/campground.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
-    title: String,
-    price: String, 
-    description: String,
-    location: String
-})
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
+});
 
-
-module.exports = mongoose.model('Campground',campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema);
